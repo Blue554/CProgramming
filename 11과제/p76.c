98f36a6cfa1eb8045b215aa625edec0b236303a6
Blue554 <112596841+Blue554@users.noreply.github.com>
2023-05-17 21:20:28 +0900

    Create p76.c

---
+++ 11과제/p76.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(void)
+{   
+system("dir");                  // dir 명령어 실행
+printf("아무 키나 치세요\n");   // 화면에 아무 키나 치세요 출력
+system("cls");                  // 화면 지우기
+return 0;
+}
