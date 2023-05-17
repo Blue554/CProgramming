6b8bb54d1286a5857fa5563bd4ab7c2d8c166bed
Blue554 <112596841+Blue554@users.noreply.github.com>
2023-05-17 21:21:58 +0900

    Update p73.c

--- 11과제/p73.c
+++ 11과제/p73.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 int main( void )
 {
-double pi = 3.1415926535;
-double x, y;
-x = pi / 2;
-y = sin( x );
-printf( "sin( %f ) = %f\n", x, y );
-y = cos( x );
-printf( "cos( %f ) = %f\n", x, y );
-}
+double pi = 3.1415926535;           // 원주율
+double x, y;                        // 각도, 삼각함수값
+x = pi / 2;                        // 90도
+y = sin( x );                       // sin(90도)
+printf( "sin( %f ) = %f\n", x, y ); // sin(90도) 출력
+y = cos( x );                       // cos(90도)
+printf( "cos( %f ) = %f\n", x, y ); // cos(90도) 출력
+} 
